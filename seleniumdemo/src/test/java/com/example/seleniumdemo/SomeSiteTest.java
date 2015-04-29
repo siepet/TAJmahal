835e21ecce8649b491ce737a7f54fa82d70e3f63
siepet <siepet7@gmail.com>
2015-04-29 16:48:36 +0200

    adding restsrvice demo

--- seleniumdemo/src/test/java/com/example/seleniumdemo/SomeSiteTest.java
+++ seleniumdemo/src/test/java/com/example/seleniumdemo/SomeSiteTest.java
@@ -16,7 +16,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class SomeSiteTest {
